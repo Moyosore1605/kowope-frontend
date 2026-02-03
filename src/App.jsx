ec4fb9174da1303cce9732503ad589c2347ce4c4
Moyosore1605 <moyosorejesuojo@gmail.com>
2026-02-03 22:34:06 +0100

    trying to fix a vercel error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import React from 'react'
 
 function App() {
 
   return (
-    <main>
+    <main className='min-h-screen w-full flex justify-center items-center'>
       <h1>Welcome to Kowope App</h1>
     </main>
   )
