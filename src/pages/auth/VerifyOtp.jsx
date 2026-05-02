e2670cfda824139a6dd2fdd6efbf6767dc6397db
Sangogade Ayomide <ayomideoluwagade@gmail.com>
2026-05-02 19:03:02 +0100

    added useEffect

--- src/pages/auth/VerifyOtp.jsx
+++ src/pages/auth/VerifyOtp.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { AlertCircle, UserCircle } from "lucide-react";
