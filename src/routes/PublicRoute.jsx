378a19718160757a2d2879660fd459f0f3d72c2b
ayomidesangogade <160060831+ayomidesangogade@users.noreply.github.com>
2026-05-04 01:37:25 +0100

    Fix loading state check in PublicRoute component

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "../hooks/useAuth";
 export default function PublicRoute() {
 	const { isAuthenticated, isLoading } = useAuth();
 
-	// if (isLoading) return <div>Loading...</div>;
+	if (isLoading) return <div>Loading...</div>;
 
 	if (isAuthenticated) {
 		return <Navigate to="/driver-dashboard" replace />;
