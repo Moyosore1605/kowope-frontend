ccfbfc49661ba9bef9a16ac0a654e7b1a55d9ad4
ayomidesangogade <160060831+ayomidesangogade@users.noreply.github.com>
2026-05-04 01:38:09 +0100

    Fix indentation for loading state in ProtectedRoute

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "../hooks/useAuth";
 export default function ProtectedRoute() {
 	const { isAuthenticated, isLoading } = useAuth();
 
-	// if (isLoading) return <div>Loading...</div>;
+	if (isLoading) return <div>Loading...</div>;
 
 	if (!isAuthenticated) {
 		return <Navigate to="/login" replace />;
